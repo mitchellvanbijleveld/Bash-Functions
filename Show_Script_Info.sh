22224127b9fb23053c05281c9d451838a19641d4
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2022-12-30 23:22:27 +0100

    Create Show_Script_Info.sh

---
+++ Show_Script_Info.sh
@@ -0,0 +1,26 @@
+###########################################################################
+# Script Information.                                                     #
+###########################################################################
+#ScriptName="Mitchell's Minecraft Server Service Installation Script"
+#ScriptDescription="Bash script that helps installing a Minecraft Server on Linux as a system service."
+#ScriptDeveloper="Mitchell van Bijleveld"
+#ScriptDeveloperWebsite="https://mitchellvanbijleveld.dev/"
+#ScriptVersion="2022 12 30 23 02 - beta"
+#ScriptCopyright="Â© 2022"
+
+ScriptName = $1
+ScriptDescription = $2
+ScriptDeveloper = $3
+ScriptDeveloperWebsite = $4
+ScriptVersion = $5
+ScriptCopyright = $6
+
+echo "$ScriptName"
+echo "$ScriptDescription"
+echo
+echo "Script Developer  : $ScriptDeveloper"
+echo "Developer Website : $ScriptDeveloperWebsite"
+echo
+echo "Version $ScriptVersion - $ScriptCopyright $ScriptDeveloper"
+echo
+###########################################################################
