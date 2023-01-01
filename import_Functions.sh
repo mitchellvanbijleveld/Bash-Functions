6dc27c07b54b1e3a2cf100d3324e30e6138da5d6
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-01 16:32:21 +0100

    Update import_Functions.sh

--- import_Functions.sh
+++ import_Functions.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Here comes the logic to verify the sha256sum of downloaded files.
+###########################################################################
+# Custom 'import' function that writes in a verbose logging style.          #
+# Mitchell van Bijleveld - (https://mitchellvanbijleveld.dev/             #
+# © 2023 Mitchell van Bijleveld. 01 / 01 / 2023                           #
+###########################################################################
 
+###########################################################################
+# Instructions.
+###########################################################################
+# It's as simple as putting 'echo_Verbose [TEXT]' instead of echo.        # 
+# In case you want to use this function only when a --verbose flag is     #
+# passed, build a function that sets an argument called                   #
+# 'ArgumentVerboseLogging' in your script (and set it to 'true'.          #
+###########################################################################
 
-
-
+import_Functions () {
 
 # Step 1 - Create a temporary directory to store the checksum files.
 TempDir="/tmp/mitchellvanbijleveld/sha256sum_Checker"
@@ -34,3 +45,4 @@ for FunctionX in $@; do
   source "$TempDir/$FunctionX.sh"
 
 done
+}
