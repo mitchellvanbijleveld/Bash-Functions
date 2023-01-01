6b7cad5036118d0e68ea2de9fecc40e786f62e2e
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-01 14:44:32 +0100

    Update echo_Verbose.sh

--- echo_Verbose.sh
+++ echo_Verbose.sh
@@ -1,9 +1,20 @@
 ###########################################################################
-# Custom Log Message Function that takes LogLevel in consideration.       #
+# Custom 'echo' function that writes in a verbose logging style.          #
+# Mitchell van Bijleveld - (https://mitchellvanbijleveld.dev/             #
+# © 2023 Mitchell van Bijleveld. 01 / 01 / 2023                           #
 ###########################################################################
+
+###########################################################################
+# Instructions.
+###########################################################################
+# It's as simple as putting 'echo_Verbose [TEXT]' instead of echo.        # 
+# In case you want to use this function only when a --verbose flag is     #
+# passed, build a function that sets an argument called                   #
+# 'ArgumentVerboseLogging' in your script (and set it to 'true'.          #
+###########################################################################
+
 echo_Verbose () {
   if $ArgumentVerboseLogging; then
     echo "LOG $(date +"%Y-%m-%d %H:%M:%S") [DEBUG] : $1"
   fi
 }
-###########################################################################
