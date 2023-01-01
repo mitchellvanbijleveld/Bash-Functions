0c2c1040495b69cc0d1bd4c0bad2129645b0a07e
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-01 16:44:34 +0100

    Update import_Functions.sh

--- import_Functions.sh
+++ import_Functions.sh
@@ -17,32 +17,35 @@
 
 import_Functions () {
 
-# Step 1 - Create a temporary directory to store the checksum files.
-TempDir="/tmp/mitchellvanbijleveld/sha256sum_Checker"
-mkdir -p $TempDir
-mkdir -p "$TempDir/sha256sum"
 
 
-# Step 2 - Download all functions, called by the script.
-# Check the arguments.
-FunctionsToCheck="$@"
-for FunctionX in $@; do
-  curl --output "$TempDir/$FunctionX.sh" "https://github.mitchellvanbijleveld.dev/Bash-Functions/$FunctionX.sh" --silent
-  curl --output "$TempDir/sha256sum/$FunctionX.sh" "https://github.mitchellvanbijleveld.dev/Bash-Functions/sha256sum/$FunctionX.sh" --silent
-  # Read the expected checksum from the checksum file
-  expected_checksum=$(cat "$TempDir/sha256sum/$FunctionX.sh")
 
-  # Verify the script's integrity
-  actual_checksum=$(sha256sum "$TempDir/$FunctionX.sh" | awk '{print $1}')
+###########################################################################
+# Step 1 - Create a temporary directory to store the checksum files.      #
+###########################################################################
+  TempDir="/tmp/mitchellvanbijleveld/sha256sum_Checker"
+  mkdir -p $TempDir
+  mkdir -p "$TempDir/sha256sum"
 
-  echo $actual_checksum
-  echo $expected_checksum
-  if [ "$expected_checksum" != "$actual_checksum" ]; then
-    echo "Error: script checksum does not match expected value"
-    exit 1
-  fi
+
+
+###########################################################################
+# Step 2 - Download all functions, called by the script.                  #
+###########################################################################
+  for FunctionX in $@; do
+    curl --output "$TempDir/$FunctionX.sh" "https://github.mitchellvanbijleveld.dev/Bash-Functions/$FunctionX.sh" --silent
+    curl --output "$TempDir/sha256sum/$FunctionX.sh" "https://github.mitchellvanbijleveld.dev/Bash-Functions/sha256sum/$FunctionX.sh" --silent
+  
+    expected_checksum=$(cat "$TempDir/sha256sum/$FunctionX.sh")
+    actual_checksum=$(sha256sum "$TempDir/$FunctionX.sh" | awk '{print $1}')
+    if [ "$expected_checksum" == "$actual_checksum" ]; then
+      return
+    else
+      echo "Error: script checksum does not match expected value"
+      exit 1
+    fi
 
 #  source "$TempDir/$FunctionX.sh"
 
-done
+  done
 }
