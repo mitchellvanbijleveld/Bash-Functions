3770683320d4106e74eb97729f318d62fdcf13ce
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-01 14:38:57 +0100

    Update print_ScriptInfo.sh

--- print_ScriptInfo.sh
+++ print_ScriptInfo.sh
@@ -1,13 +1,22 @@
 ###########################################################################
-# Script Information.                                                     #
+# Print information about a script                                        #
+# Mitchell van Bijleveld - (https://mitchellvanbijleveld.dev/             #
+# © 2023 Mitchell van Bijleveld. 01 / 01 / 2023                           #
 ###########################################################################
-# Please set the following variables in the script you want to use:
-# ScriptName             : Name of the script you use
-# ScriptDescription      : Description of the script with a brief explaination of what the script does.
-# ScriptDeveloper        : Name of the developer
-# ScriptDeveloperWebsite : Website of the developer
-# ScriptVersion          : Version of your script
-# ScriptCopyright        : Year of copyright
+
+###########################################################################
+# Instructions.
+###########################################################################
+# Please set the following variables in the script you want to use:       #
+# ScriptName             : Name of the script you use                     #
+# ScriptDescription      : Description of the script with a brief         #
+#                          explaination of what the script does.          #
+# ScriptDeveloper        : Name of the developer                          #
+# ScriptDeveloperWebsite : Website of the developer                       #
+# ScriptVersion          : Version of your script                         #
+# ScriptCopyright        : Year of copyright                              # 
+###########################################################################
+
 print_ScriptInfo () {
   echo "$ScriptName"
   echo "$ScriptDescription"
@@ -18,4 +27,3 @@ print_ScriptInfo () {
   echo "Version $ScriptVersion - $ScriptCopyright $ScriptDeveloper"
   echo
 }
-###########################################################################
