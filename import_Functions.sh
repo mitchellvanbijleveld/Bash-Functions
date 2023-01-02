8fb6b53414ff10f7ddebea87bd45841a7f5a67b0
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-02 01:03:47 +0100

    Update import_Functions.sh

--- import_Functions.sh
+++ import_Functions.sh
@@ -36,7 +36,7 @@ import_Functions () {
   mkdir -p "$TempDir/sha256sum"
 
   TerminalWidth=$(tput cols)
-  ProgressBarStepSize=$(($TerminalWidth-6))
+  ProgressBarStepSize=$(($TerminalWidth-4))
   ProgressBarStepSize=$(($ProgressBarStepSize/$#))
   ProgressBarStepSize=$(($ProgressBarStepSize/6))
 
