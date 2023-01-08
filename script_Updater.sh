ae703a13de065d2692b9123cbd7282462442dfa8
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-08 23:51:08 +0100

    Update script_Updater.sh

--- script_Updater.sh
+++ script_Updater.sh
@@ -36,9 +36,9 @@ Check_Script_Update () {
     echo "Restarting Script from '$ScriptPath' with arguments '$@' in 5 seconds..."
     sleep 5
     if [[ $@ == "" ]]; then
-      source $ScriptPath
+      /usr/bin/bash $ScriptPath
     else
-      source $ScriptPath $@
+      /usr/bin/bash $ScriptPath $@
     fi
     exit
   elif [[ $ScriptVersion > $Online_ScriptVersion ]]; then
