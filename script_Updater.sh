b727946bd7e45e2b427909e60ec8cc6ca3d2f1ac
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-08 23:36:31 +0100

    Update script_Updater.sh

--- script_Updater.sh
+++ script_Updater.sh
@@ -35,7 +35,7 @@ Check_Script_Update () {
     echo
     echo "Restarting Script from '$ScriptPath' with arguments '$@' in 5 seconds..."
     sleep 5
-    /usr/bin/bash "$ScriptPath $@"
+    /usr/bin/bash "$ScriptPath$@"
     exit
   elif [[ $ScriptVersion > $Online_ScriptVersion ]]; then
     echo -e "\x1B[1;33mYour version of the script ($ScriptVersion) is newer than the server version ($Online_ScriptVersion).\x1B[0m\n"
