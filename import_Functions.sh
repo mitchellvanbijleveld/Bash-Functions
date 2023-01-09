77accf7b8e2515a2be9b279c1e967fa505acf065
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-09 21:46:18 +0100

    Update import_Functions.sh

--- import_Functions.sh
+++ import_Functions.sh
@@ -107,19 +107,20 @@ fi
       vTempFunction=$(echo $vTempFunction | sed 's/ //g')
       if [[ $vFunction != $vTempFunction ]]; then
         curl --output "$TempDir/$FunctionX.sh" "https://github.mitchellvanbijleveld.dev/Bash-Functions/$FunctionX.sh" --silent &
+        wait
+        vTempFunction=$(cat "$TempDir/$FunctionX.sh" | grep "##### Version")
+        vTempFunction=$(echo $vTempFunction | sed 's/#//g')
+        vTempFunction=$(echo $vTempFunction | sed 's/Version//g')
+        vTempFunction=$(echo $vTempFunction | sed 's/ //g')
+        if [[ $vFunction != $vTempFunction ]]; then
+          UpdateProgressBar --finish-progressbar
+          unset echo
+          echo "Fatal error: version mismatch during import one of the functions."
+          exit
+        fi
       fi
     else
       curl --output "$TempDir/$FunctionX.sh" "https://github.mitchellvanbijleveld.dev/Bash-Functions/$FunctionX.sh" --silent &
-      vTempFunction=$(cat "$TempDir/$FunctionX.sh" | grep "##### Version")
-      vTempFunction=$(echo $vTempFunction | sed 's/#//g')
-      vTempFunction=$(echo $vTempFunction | sed 's/Version//g')
-      vTempFunction=$(echo $vTempFunction | sed 's/ //g')
-      if [[ $vFunction != $vTempFunction ]]; then
-        UpdateProgressBar --finish-progressbar
-        unset echo
-        echo "Fatal error: version mismatch during import one of the functions."
-        exit
-      fi
     fi
     UpdateProgressBar
     
