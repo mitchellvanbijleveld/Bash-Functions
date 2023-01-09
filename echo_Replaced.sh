a14a811f56095511c72f8637f635a9c8888f8135
Mitchell van Bijleveld <106330077+mitchellvanbijleveld@users.noreply.github.com>
2023-01-09 20:55:49 +0100

    Update echo_Replaced.sh

--- echo_Replaced.sh
+++ echo_Replaced.sh
@@ -2,6 +2,7 @@
 # Custom 'echo' function that writes in a verbose logging style.          #
 # Mitchell van Bijleveld - (https://mitchellvanbijleveld.dev/             #
 # © 2023 Mitchell van Bijleveld. 01 / 01 / 2023                           #
+##### Version 0.1.0                                                       #
 ###########################################################################
 
 ###########################################################################
